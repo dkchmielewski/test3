124b8444323a0463332f9d22793d74fe0be5ca84
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-03 07:57:08 -0400

    Added name

--- src/test3.java
+++ src/test3.java
@@ -5,5 +5,7 @@ public class test3 {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("nowy");
+        System.out.println("Daniel");
     }
 }
